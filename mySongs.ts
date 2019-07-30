81e9988e5e86b9628d9d25eadb02dec4ed6ae80c
colemaana <52672416+colemaana@users.noreply.github.com>
2019-07-29 22:13:59 -0400

    Add files via upload

---
+++ mySongs.ts
@@ -0,0 +1,9 @@
+import { colemaan } from "src/app/colemaan";
+import {songs} from "src/app/songInfo";
+
+export const MYSONGS: songs[] = [
+    {songname: 'heyya', songartist: 'outkast',songgenre:'',songpic:'',songyear: 2001},
+    {songname: 'O', songartist: 'omarion',songgenre:'r&b',songpic:'./images/badminton.jpg',songyear: 2001},
+    {songname: 'yo', songartist: 'hi',songgenre:'',songpic:'',songyear: 2001},
+    {songname: 'yo', songartist: 'hi',songgenre:'',songpic:'',songyear: 2001}
+];
